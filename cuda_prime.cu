6bd575456e4816f5636ba182f7f5f7cf34a838ca
Wojciech Richert <33549959+wojtekrichert@users.noreply.github.com>
2017-12-24 01:01:28 +0100

    Add files via upload

--- cuda_prime.cu
+++ cuda_prime.cu
@@ -7,21 +7,24 @@ using namespace std;
 __global__
 void lista_pierwszych( unsigned long long int *d_sqrtzliczby,
                        unsigned long long int *d_liczba,
-                       bool *d_jest){
-  long int i = (blockIdx.x*blockDim.x + threadIdx.x);
-  if(i<*d_sqrtzliczby+100 && i>2 && i%2!=0){
-    if(*d_liczba % i==0){
-      *d_jest =1 ;
+                       bool *d_jest,
+                       int *current_thread_count){
+  atomicAdd(current_thread_count, 1);
+    long int i = (blockIdx.x*blockDim.x + threadIdx.x);
+    if(i<*d_sqrtzliczby+100 && i>2 && i%2!=0){
+      if(*d_liczba % i==0){
+        *d_jest =1 ;
+      }
     }
-  }
 }
+
 
 int liczbapierwsza() {
   clock_t begin = clock();
 	bool z=1;
 	long  y=2;
 	int i = 2;
-	unsigned long long int x=10000001400000049;
+	unsigned long long int x=281200132374529;
 		while (y < sqrt(x)+10) {
 			int flag = 0;
 			for (i; i <= i / 2; i++){
@@ -40,14 +43,14 @@ int liczbapierwsza() {
 			y++;
 		}
 		if (z == 0) {
-			printf(" nie jestliczba pierwsza ");
+			printf("%llu nie jest liczba pierwsza\n ",x);
 		}
 		else
-			printf(" jest ");
+			printf("%llu jest liczba pierwsza\n ",x);
 
       clock_t end = clock();
       double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-      printf("%f ", elapsed_secs);
+      printf("czas CPU: %f \n", elapsed_secs);
 	return 1;
 }
 
@@ -55,7 +58,10 @@ int main(void){
   liczbapierwsza();
   unsigned long long *liczba, *d_liczba, *sqrtzliczby, *d_sqrtzliczby;
   bool *jest, *d_jest;
-
+  int tally, *dev_tally;
+  cudaMalloc((void **)&dev_tally, sizeof(int));
+  tally = 0;
+  cudaMemcpy(dev_tally, &tally, sizeof(int), cudaMemcpyHostToDevice);
   jest= (bool*)malloc(sizeof(bool));
   liczba = (unsigned long long*)malloc(sizeof(unsigned long long));
   sqrtzliczby =(unsigned long long*)malloc(sizeof(unsigned long long));
@@ -63,32 +69,32 @@ int main(void){
   cudaMalloc(&d_sqrtzliczby, sizeof(unsigned long long));
   cudaMalloc(&d_jest,sizeof(unsigned long long));
 
-  *liczba = 10000001400000049;
+  *liczba = 281200132374529;
   *sqrtzliczby = sqrtl(*liczba);
-  printf("%llu ", *sqrtzliczby);
+  printf("pierwiastek z liczby: %llu\n ", *sqrtzliczby);
   cudaMemcpy(d_liczba,liczba,sizeof(unsigned long long), cudaMemcpyHostToDevice);
   cudaMemcpy(d_sqrtzliczby,sqrtzliczby, sizeof(unsigned long long),cudaMemcpyHostToDevice );
   cudaMemcpy(d_jest, jest, sizeof(bool), cudaMemcpyHostToDevice);
+
   clock_t begin = clock();
   if(*liczba%2 ==0){
-    printf("%llu ", *liczba);
-    printf("nie jest liczba pierwsza");
+    printf("%llu nie jest liczba pierwsza\n", *liczba);
   }
   else{
-    lista_pierwszych<<<((int)sqrt(*liczba)+255)/256, 256>>>( d_sqrtzliczby,d_liczba,d_jest);
+    lista_pierwszych<<<((int)sqrt(*liczba)+255)/256, 256>>>( d_sqrtzliczby,d_liczba,d_jest, dev_tally);
     cudaMemcpy(jest, d_jest,sizeof(bool),cudaMemcpyDeviceToHost);
+    cudaMemcpy(&tally, dev_tally, sizeof(int), cudaMemcpyDeviceToHost);
+    printf("total number of threads that executed was: %d\n", tally);
     if(*jest == 0){
-      printf("%llu ", *liczba);
-      printf("jest liczba pierwsza");
+      printf("%llu jest liczba pierwsza\n", *liczba);
     }
     else{
-      printf("%llu ", *liczba);
-      printf("nie jest liczba pierwsza");
+      printf("%llu nie jest liczba pierwsza\n", *liczba);
     }
   }
   clock_t end = clock();
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-  printf(" %f ", elapsed_secs);
+  printf("czas GPU: %f\n ", elapsed_secs);
 
   cudaFree(d_liczba);
   cudaFree(d_sqrtzliczby);
